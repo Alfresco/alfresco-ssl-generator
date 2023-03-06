791b6a9729567db0cda6946bf91445523ddd4fc2
mstrankowski <marcin.strankowski@hyland.com>
2023-03-06 23:47:09 +0100

    Add simple test script for windows version

---
+++ ssl-tool-win/test_simple.cmd
@@ -0,0 +1,26 @@
+rd /s /q ca
+rd /s /q certificates
+rd /s /q keystores
+
+REM CA, Repository, Solr, Zeppelin
+call run.cmd -alfrescoversion community -keysize 2048 -keystoretype JCEKS -truststoretype JCEKS -keystorepass kT9X6oe68t -truststorepass kT9X6oe68t -encstorepass mp6yc0UD9e -encmetadatapass oKIWzVdEdA -alfrescoformat current
+
+REM Shared file store
+call run_additional.cmd -servicename sharedFileStore -alias sharedFileStore -keysize 2048 -keystoretype JCEKS -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname  "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=Shared File Store" -servername localhost -alfrescoformat current
+REM Transform Router
+call run_additional.cmd -servicename transformRouter -alias transformRouter -keysize 2048 -keystoretype JCEKS -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname  "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=Transform Router" -servername localhost -alfrescoformat current
+REM T-Engine AIO
+call run_additional.cmd -servicename tengineAIO -alias tengineAIO -keysize 2048 -keystoretype JCEKS -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname  "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=T-Engine AIO" -servername localhost -alfrescoformat current
+REM T-Engine Imagemagick
+call run_additional.cmd -servicename tengineImageMagick -alias tengineImageMagick -keysize 2048 -keystoretype JCEKS -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname  "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=T-Engine ImageMagick" -servername localhost -alfrescoformat current
+REM T-Engine Libreoffice
+call run_additional.cmd -servicename tengineLibreOffice -alias tengineLibreOffice -keysize 2048 -keystoretype JCEKS -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname  "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=T-Engine LibreOffice" -servername localhost -alfrescoformat current
+REM T-Engine Pdfrenderer
+call run_additional.cmd -servicename tenginePdfRenderer -alias tenginePdfRenderer -keysize 2048 -keystoretype JCEKS -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname  "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=T-Engine PdfRenderer" -servername localhost -alfrescoformat current
+REM T-Engine Tika
+call run_additional.cmd -servicename tengineTika -alias tengineTika -keysize 2048 -keystoretype JCEKS -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname  "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=T-Engine Tika" -servername localhost -alfrescoformat current
+REM T-Engine Misc
+call run_additional.cmd -servicename tengineMisc -alias tengineMisc -keysize 2048 -keystoretype JCEKS -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname  "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=T-Engine Misc" -servername localhost -alfrescoformat current
+
+REM Custom T-Engine
+call run_additional.cmd -servicename tengineCustom -alias tengineCustom -keysize 2048 -keystoretype JCEKS -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname  "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=T-Engine Custom" -servername localhost -alfrescoformat current

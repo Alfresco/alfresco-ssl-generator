c1dcdd69e26c757f11941f6e37ef3f068efc114d
mstrankowski <marcin.strankowski@hyland.com>
2023-03-17 09:13:43 +0100

    Linux removal of unused parameter in samples

--- ssl-tool/samples/new_client_server.sh
+++ ssl-tool/samples/new_client_server.sh
@@ -30,7 +30,7 @@ fi
 cp $KEYSTORES_DIR/solr/solr.keystore $ZEPPELIN_DIR/zeppelin.keystore
 cp $KEYSTORES_DIR/solr/solr.truststore $ZEPPELIN_DIR/zeppelin.truststore
 #Solr browser
-./../run_additional.sh -subfoldername client -servicename browser -role client -rootcapass kT9X6oe68t -keysize 2048 -keystoretype PKCS12 -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=Custom Browser Client" -servername localhost -alfrescoformat $ALFRESCO_FORMAT
+./../run_additional.sh -subfoldername client -servicename browser -role client -rootcapass kT9X6oe68t -keysize 2048 -keystoretype PKCS12 -keystorepass kT9X6oe68t -truststoretype JCEKS -truststorepass kT9X6oe68t -certdname "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=Custom Browser Client" -alfrescoformat $ALFRESCO_FORMAT
 rm $KEYSTORES_DIR/client/browser_client.truststore
 rm $KEYSTORES_DIR/client/browser_client-truststore-passwords.properties
 

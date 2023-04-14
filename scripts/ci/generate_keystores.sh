3bad21bffc468b3b77137c79b71b2ce93848dec7
mstrankowski <marcin.strankowski@hyland.com>
2023-04-14 20:45:14 +0200

    Forgotten AWS AI keystore instruction used for ai-renditions

--- scripts/ci/generate_keystores.sh
+++ scripts/ci/generate_keystores.sh
@@ -45,5 +45,8 @@ bash ${SCRIPT_DIR}/../../ssl-tool/run_additional.sh -servicename tengineMisc -ro
 #Transform Aspose
 bash ${SCRIPT_DIR}/../../ssl-tool/run_additional.sh -servicename tAspose -rootcapass password -keysize 2048 -keystoretype JCEKS -keystorepass password -truststoretype JCEKS -truststorepass password -certdname "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=Transform Aspose" -servername localhost,transform-aspose -alfrescoformat $ALFRESCO_FORMAT
 
+#AWS AI
+bash ${SCRIPT_DIR}/../../ssl-tool/run_additional.sh -servicename awsAi -rootcapass password -keysize 2048 -keystoretype JCEKS -keystorepass password -truststoretype JCEKS -truststorepass password -certdname "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=AWS AI" -servername localhost,aws-ai -alfrescoformat $ALFRESCO_FORMAT
+
 #HttpClient used in tests
 bash ${SCRIPT_DIR}/../../ssl-tool/run_additional.sh -servicename testClient -rootcapass password -keysize 2048 -keystoretype JCEKS -keystorepass password -truststoretype JCEKS -truststorepass password -certdname "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=Test Client" -servername localhost,test-client -alfrescoformat $ALFRESCO_FORMAT

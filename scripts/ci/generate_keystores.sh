8721ec0e3c340031839ee701522098217a627170
cezary-witkowski <cezary.witkowski@hyland.com>
2026-04-10 10:04:22 +0200

    [ACS-7965] Add elasticsearch and liveIndexing

--- scripts/ci/generate_keystores.sh
+++ scripts/ci/generate_keystores.sh
@@ -43,6 +43,11 @@ bash ${SCRIPT_DIR}/../../ssl-tool/run_additional.sh -servicename tengineMisc -ro
 #Transform Aspose
 bash ${SCRIPT_DIR}/../../ssl-tool/run_additional.sh -servicename tAspose -rootcapass password -keysize 2048 -keystoretype JCEKS -keystorepass password -truststoretype JCEKS -truststorepass password -certdname "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=Transform Aspose" -servername localhost,transform-aspose -alfrescoformat $ALFRESCO_FORMAT
 
+#Elasticsearch
+bash ${SCRIPT_DIR}/../../ssl-tool/run_additional.sh -servicename elasticsearch -rootcapass password -keysize 2048 -keystoretype JCEKS -keystorepass password -truststoretype JCEKS -truststorepass password -certdname "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=Elasticsearch" -servername localhost,elasticsearch -alfrescoformat $ALFRESCO_FORMAT
+#Live Indexing
+bash ${SCRIPT_DIR}/../../ssl-tool/run_additional.sh -servicename liveIndexing -rootcapass password -keysize 2048 -keystoretype JCEKS -keystorepass password -truststoretype JCEKS -truststorepass password -certdname "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=Live Indexing" -servername localhost,live-indexing -alfrescoformat $ALFRESCO_FORMAT
+
 #AWS AI
 bash ${SCRIPT_DIR}/../../ssl-tool/run_additional.sh -servicename awsAi -rootcapass password -keysize 2048 -keystoretype JCEKS -keystorepass password -truststoretype JCEKS -truststorepass password -certdname "/C=GB/ST=UK/L=Maidenhead/O=Alfresco Software Ltd./OU=Unknown/CN=AWS AI" -servername localhost,aws-ai -alfrescoformat $ALFRESCO_FORMAT
 
